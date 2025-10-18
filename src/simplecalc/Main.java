127a54c2583ed7d2baac96e09a1c25e1558758a6
Jorge <Jorge@DESKTOP-BQG6K35.mshome.net>
2025-10-18 13:54:39 -0600

    se agrega el HEAD

--- src/simplecalc/Main.java
+++ src/simplecalc/Main.java
@@ -9,15 +9,10 @@ Salgado Rojas Marelin Iral
 Lenguajes y Automatas II
 Profa: Martínez Moreno Martha
 
-Descripción: programa que maneja etapas lexica, 
-sintactica, semantica por validación de entradas
-Transformación de expresiones aritmeticas de
-infija a prefija, uso de cuadruplos en el 
-algoritmo de traducción y conversión por partes
-Generacón de código intermedio en expresiones 
-aritmeticas, transormado de infija a prefija, 
-uso de cuadruplos, y manejo temporal de variables
-
+Avance 2: se genera la validación por código intermedio
+Transforma operaciones aritméticas de infija a prefija
+Utiliza cuadruplos para manejar las operaciones aritmeticas
+Almacena variables temporales para asignar los valores a variables declaradas
 Branch: Prefija.
 
 
